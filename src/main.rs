ba138f1b9d2d0b2ca99d3c2872ec1b259ca3db44
ajesipow <32611784+ajesipow@users.noreply.github.com>
2024-03-03 10:52:30 +0100

    No tracing

--- src/main.rs
+++ src/main.rs
@@ -8,13 +8,6 @@ mod protocol;
 mod transport;
 
 fn main() -> Result<()> {
-    // TODO change DIR here
-    let file_appender =
-        tracing_appender::rolling::hourly("/Users/alexjesipow/coding/gossip", "echo.log");
-    tracing_subscriber::fmt()
-        .with_max_level(Level::DEBUG)
-        .with_writer(file_appender)
-        .init();
     let transport = StdInTransport::new();
     let mut node = Node::new(transport);
     node.run()
